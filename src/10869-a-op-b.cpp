182a688d2110013d449e894ee93ba03be772e2ff
danichoi737 <danichoi737@gmail.com>
2023-12-18 07:59:42 +0900

    Solve 10869

---
+++ src/10869-a-op-b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  unsigned short a {};
+  unsigned short b {};
+  std::cin >> a >> b;
+
+  std::cout << a + b << std::endl;
+  std::cout << a - b << std::endl;
+  std::cout << a * b << std::endl;
+  std::cout << a / b << std::endl;
+  std::cout << a % b << std::endl;
+
+  return 0;
+}
