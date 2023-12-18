783451c63279444b7e559ab287f6cfcedbd73adf
danichoi737 <danichoi737@gmail.com>
2023-12-19 01:50:27 +0900

    Solve 14681

---
+++ src/14681-quadrant.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+  short x {};
+  short y {};
+  std::cin >> x;
+  std::cin >> y;
+
+  if (x > 0) {
+    if (y > 0) {
+      std::cout << "1" << std::endl;
+    }
+    else {
+      std::cout << "4" << std::endl;
+    }
+  }
+  else {
+    if (y > 0) {
+      std::cout << "2" << std::endl;
+    }
+    else {
+      std::cout << "3" << std::endl;
+    }
+  }
+
+  return 0;
+}
