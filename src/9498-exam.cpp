e2e4682b5540be3955d30f0fa5ea8f945df60c2e
danichoi737 <danichoi737@gmail.com>
2023-12-19 01:31:30 +0900

    Solve 9498

---
+++ src/9498-exam.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+  short score {};
+  std::cin >> score;
+
+  if (score >= 90 && score <= 100) {
+    std::cout << "A" << std::endl;
+  }
+  else if (score >= 80 && score < 90) {
+    std::cout << "B" << std::endl;
+  }
+  else if (score >= 70 && score < 80) {
+    std::cout << "C" << std::endl;
+  }
+  else if (score >= 60 && score < 70) {
+    std::cout << "D" << std::endl;
+  }
+  else {
+    std::cout << "F" << std::endl;
+  }
+
+  return 0;
+}
