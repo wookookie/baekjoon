3e679a3d92ec12e1e288f1e429260431800d5b0e
danichoi737 <danichoi737@gmail.com>
2023-12-19 00:32:53 +0900

    Solve 11382

---
+++ src/11382-jungmin.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  unsigned long long a {};
+  unsigned long long b {};
+  unsigned long long c {};
+  std::cin >> a >> b >> c;
+
+  std::cout << a + b + c << std::endl;
+
+  return 0;
+}
